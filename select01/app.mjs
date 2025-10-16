405ee8c0b39b88eec605349a2727cf25a0d7264c
kalifaszi01 <petbal586@logiker.hu>
2025-10-16 13:36:08 +0200

    gyumolcs 1.2

--- select01/app.mjs
+++ select01/app.mjs
@@ -2,3 +2,6 @@ import gyumolcsok from './gyumolcsok.json' with { type: 'json' };
 console.log(gyumolcsok);
 let rendezett_gyumolcsok=[...gyumolcsok];
 console.log(rendezett_gyumolcsok);
+console.log("Készleten lévő gyümölcsök.");
+let keszleten_gyumolcsok = rendezett_gyumolcsok.filter(g => g.keszleten).map(gyumolcsok => gyumolcsok.neve);
+console.log(keszleten_gyumolcsok);
